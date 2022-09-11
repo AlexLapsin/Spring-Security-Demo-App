36aa66235f8d79811053534c036d90042394231f
Alex <alexandrulapshin@gmail.com>
2022-09-10 22:40:31 -0400

    Add 3 inMemory users and use default login form(from spring)

--- spring-security-demo-01-base-app/src/main/java/com/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-01-base-app/src/main/java/com/springsecurity/demo/config/DemoSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -13,23 +16,37 @@ public class DemoSecurityConfig{
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    		
-        return http
-		.authorizeRequests(configurer ->
-			configurer
-				.anyRequest()
-				.authenticated())
-		
-		.formLogin(configurer ->
-			configurer
-				.loginPage("/showMyLoginPage")
-				.loginProcessingUrl("/authenticateTheUser")
-				.permitAll())
-		
-		.build();  
-    }	
+        http
+            .authorizeHttpRequests((authz) -> authz
+                .anyRequest().authenticated()
+            )
+            .formLogin();
+        return http.build();
+    }
 
-	
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+    	
+        UserDetails john = User.builder()
+            .username("john")
+            .password("{noop}test123")
+            .roles("EMPLOYEE")
+            .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("ADMIN")
+                .build();
+        
+        return new InMemoryUserDetailsManager(john, mary, susan);  
+    }
 	
 	
 }
